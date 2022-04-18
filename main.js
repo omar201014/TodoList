ddb4be30545c6270b1ee8412e22d67efd9843916
omar201014 <omar_hussein201014@hotmail.com>
2022-04-18 21:09:16 +0200

    task-intiate

---
+++ main.js
@@ -0,0 +1,51 @@
+// global scope variables //
+const form = document.getElementById('myForm');
+const tasks = [];
+// setting up event listeners for submit //
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log("form is submitting ...");
+    addTask(e.target.taskName.value , e.target.taskType.value);
+    createTask();
+    } 
+)      
+
+// add tasks with the given data to the array//
+
+function addTask(name , type) {
+    const task = {
+        taskName: name,
+        taskType: type       
+    }
+    tasks.push(task);
+    console.table(tasks);
+}
+// create a task with the given data from the array//
+
+function createTask() {
+    const taskList = document.getElementById('taskContainer');
+    const task = document.createElement('li');
+    const completeBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
+    completeBtn.innerText = 'Complete Task';
+    completeBtn.classList.add('btn','btn-success' ,'mx-2');
+    removeBtn.innerText = 'Remove Task';
+    removeBtn.classList.add('btn','btn-danger' ,'mx-2');
+    task.classList.add('list-group-item');
+    task.innerHTML = `<h1>${tasks[tasks.length - 1].taskName}</h1>
+    <h4>${tasks[tasks.length - 1].taskType}</h4>`;
+    task.append(completeBtn);
+    task.append(removeBtn);
+    taskList.append(task);
+    // green button action //
+    completeBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        e.target.parentElement.classList.remove('list-group-item');
+        e.target.parentElement.classList.add('alert' , 'alert-success');
+        })
+    // red button action //
+    removeBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        e.target.parentElement.remove();
+        })
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# To Do list 
+
+In this tutorial i used **bootstrap** and **Javascipt** to create a dynamic list with the given data 
